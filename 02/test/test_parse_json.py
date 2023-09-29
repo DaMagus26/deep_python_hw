34fd6c24c0149dd9a7ab12cfd5bb75928417f4e2
Michael Ovakimian <mov.express17@gmail.com>
2023-09-29 11:58:07 +0300

    Added test files

---
+++ 02/test/test_parse_json.py
@@ -0,0 +1,9 @@
+import unittest
+from unittest import mock
+import sys
+sys.path.append('../')
+from parse_json import parse_json
+
+
+class TestParseJson(unittest.TestCase):
+    pass
---
+++ 02/test/test_timer.py
@@ -0,0 +1,9 @@
+import unittest
+from unittest import mock
+import sys
+sys.path.append('../')
+from timer import mean
+
+
+class TestTimer(unittest.TestCase):
+    pass
